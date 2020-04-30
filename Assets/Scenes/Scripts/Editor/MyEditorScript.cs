0f34d6fbb8edacd453fab887c9f40fe684e56788
leelai <aloha.app4u@gmail.com>
2020-04-30 13:52:11 +0800

    Add build script

---
+++ Assets/Scenes/Scripts/Editor/MyEditorScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+class MyEditorScript
+{
+    static void PerformBuild()
+    {
+        string[] scenes = { "Assets/Scenes/SampleScene.unity" };
+        BuildPipeline.BuildPlayer(scenes, "Build/Win/Test.exe",
+             BuildTarget.StandaloneWindows64,
+             BuildOptions.None);
+    }
+}
